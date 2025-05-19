b6319ea1036fe77c697690ca8028c1a732e6c92d
Nina Rosliakova <nrosliakova@kse.org.ua>
2025-05-19 12:40:35 +0300

    add struct node add resize func

--- TextEditor/TextEditor.cpp
+++ TextEditor/TextEditor.cpp
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct node{
 	char value = NULL;
 	node* next = NULL;
 } node;
 
-int main() {
+node* resize(node* ptr, int new_size);
 
+int arraysize = 4;
+node last_char;
+
+int main() {
+	//---
+	node* p_first_chars;
+	node* p_last_chars;
+
+	p_first_chars = (node*)calloc(arraysize, sizeof(node));
+	//p_last_chars = (node*)calloc(arraysize, sizeof(node));
+
+	//---
+	printf("'%c'\n", last_char.value);
 	printf("Hello User!\n");
 	char command;
 
@@ -30,25 +44,25 @@ int main() {
 				);
 				break;
 			case '1': // Append text symbols to the end 
-				printf("The command is not implemented\n");
+				printf("The command 1 is not implemented\n");
 				break;
 			case '2': // Start the new line
-				printf("The command is not implemented\n");
+				printf("The command 2 is not implemented\n");
 				break;
 			case '3': // Use files to saving the information
-				printf("The command is not implemented\n");
+				printf("The command 3 is not implemented\n");
 				break;
 			case '4': // Use files to loading the information
-				printf("The command is not implemented\n");
+				printf("The command 4 is not implemented\n");
 				break;
 			case '5': // Print the current text to console 
-				printf("The command is not implemented\n");
+				printf("The command 5 is not implemented\n");
 				break;
 			case '6': // Insert the text by line and symbol index
-				printf("The command is not implemented\n");
+				printf("The command 6 is not implemented\n");
 				break;
 			case '7': // Search (please note that text can be found more than once)
-				printf("The command is not implemented\n");
+				printf("The command 7 is not implemented\n");
 				break;
 			case 'e':
 				return 0;
@@ -59,9 +73,14 @@ int main() {
 		while (getchar() != '\n');
 		getchar();
 		system("cls");
+		while (getchar() != '\n');
 	}
 	
 	
 	return 0;
 }
 
+node* resize(node* ptr, int new_size) {
+	node* ptr2 = (node* )realloc(ptr, new_size);
+	return ptr2;
+}
