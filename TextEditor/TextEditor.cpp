724e0d6083917b2b8edae1bde0cac3bddcdaf723
Nina Rosliakova <nrosliakova@kse.org.ua>
2025-05-25 00:42:09 +0300

    can append multiple times and print all text

--- TextEditor/TextEditor.cpp
+++ TextEditor/TextEditor.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 typedef struct node{
@@ -7,21 +8,36 @@ typedef struct node{
 	node* next = NULL;
 } node;
 
-node* resize(node* ptr, int new_size);
+//node* resize(node* ptr, int new_size);
+node* resize();
+//void append(const char text[]);
+void print_text();
+void clean_buffer();
 
 int arraysize = 4;
-node last_char;
+node* p_first_chars;
+node* last_char;
+int line_counter = 1;
 
 int main() {
 	//---
-	node* p_first_chars;
-	node* p_last_chars;
+	//node* p_first_chars;
+	//node* p_last_chars;
 
 	p_first_chars = (node*)calloc(arraysize, sizeof(node));
 	//p_last_chars = (node*)calloc(arraysize, sizeof(node));
+	//last_char.value = NULL;
+	//last_char.next = NULL;
+	for (int i = 0; i < arraysize; i++) {
+		node n;
+		//n.value = NULL;
+		//n.next = NULL;
+		p_first_chars[i] = n;
+	}
 
 	//---
-	printf("'%c'\n", last_char.value);
+	last_char = p_first_chars;
+	//printf("'%c'\n", last_char.value);
 	printf("Hello User!\n");
 	char command;
 
@@ -44,10 +60,52 @@ int main() {
 				);
 				break;
 			case '1': // Append text symbols to the end 
-				printf("The command 1 is not implemented\n");
+			{
+				//printf("The command 1 is not implemented\n");
+				char text[100];
+				printf("Write text to append (up to 100 characters):\n\n");
+				//clean_buffer();
+				while (getchar() != '\n');
+				fgets(text, sizeof(char)*100, stdin);
+				int len = strlen(text);
+				if (len > 0 && text[len - 1] == '\n') {
+					text[len - 1] = '\0';
+				}
+				text[len] = '\0';
+				len -= 1;
+				//append(text);
+				//---------
+				
+				node* nodes = (node*)calloc(len+1, sizeof(node));
+				bool execute_if = true;
+				for (int i = 0; i < len ; i++) {
+					node* cur_node_p;
+					if (last_char->value != NULL && i == 0 && execute_if) {
+						last_char->next = &nodes[i];
+						cur_node_p = last_char;
+						i--;
+						execute_if = false;
+						continue;
+					}
+					cur_node_p = &nodes[i];
+					cur_node_p->value = text[i];
+					
+					node* node_next_p = &nodes[i+1];
+					cur_node_p->next = (i != len - 1) ? node_next_p : NULL;
+					last_char = cur_node_p;
+					if (p_first_chars[line_counter-1].value == NULL && i == 0) 
+						p_first_chars[line_counter-1] = *cur_node_p;
+				}
+				//---------
+				printf("\n");
+				printf("Done! Text to append:\n%s\n", text);
 				break;
+			}
 			case '2': // Start the new line
-				printf("The command 2 is not implemented\n");
+				//printf("The command 2 is not implemented\n");
+				//append("\n");
+				line_counter++;
+				printf("Done!\n");
 				break;
 			case '3': // Use files to saving the information
 				printf("The command 3 is not implemented\n");
@@ -56,7 +114,26 @@ int main() {
 				printf("The command 4 is not implemented\n");
 				break;
 			case '5': // Print the current text to console 
-				printf("The command 5 is not implemented\n");
+				//printf("The command 5 is not implemented\n");
+				//print_text();
+				//---------
+				printf("Your text:\n\n");
+				for (int i = 0; i < line_counter; i++) {
+
+					node cur_char = p_first_chars[i];
+					while (cur_char.value != NULL) {
+						printf("%c", cur_char.value);
+						if (cur_char.next != NULL)
+							cur_char = *cur_char.next;
+						else break;
+					}
+
+
+					printf("\n");
+				}
+				//---------
+				printf("\n");
+
 				break;
 			case '6': // Insert the text by line and symbol index
 				printf("The command 6 is not implemented\n");
@@ -70,17 +147,63 @@ int main() {
 				printf("Error! The command '%c' does not exist.\n", command);
 		}
 		printf("\nPress enter to continue. ");
-		while (getchar() != '\n');
-		getchar();
+		//while (getchar() != '\n');
+		//getchar();
+		clean_buffer();
 		system("cls");
-		while (getchar() != '\n');
+		//while (getchar() != '\n');
 	}
 	
 	
 	return 0;
 }
 
-node* resize(node* ptr, int new_size) {
-	node* ptr2 = (node* )realloc(ptr, new_size);
+//node* resize(node* ptr) {
+node* resize() {
+	//node* ptr2 = (node* )realloc(ptr, arraysize * sizeof(node*));
+	node* ptr2 = (node* )realloc(p_first_chars, arraysize * sizeof(node*));
 	return ptr2;
+}
+
+//void append(const char text[]) {
+//	int len = strlen(text);
+//	node prev_node = last_char;
+//	for (int i = 0; i < len; i++) {
+//		node cur_node;
+//		cur_node.value = text[i];
+//		if (cur_node.value == NULL) i = len - 1;
+//		//prev_node.next = &cur_node;
+//		node* ptr = (node*)malloc(sizeof(node));
+//		ptr = &cur_node;
+//		prev_node.next = ptr;
+//		if (i == len - 1)
+//			last_char = cur_node;
+//	}
+//}
+
+void print_text() {
+	for (int i = 0; i < line_counter; i++) {
+		//node cur_char = p_first_chars[i];
+		//do {
+		//	printf("%c", cur_char.value);
+		//	cur_char = *cur_char.next;
+		//} while (cur_char.value != NULL);
+
+		node cur_char = *p_first_chars[i].next;
+		while (cur_char.value != NULL) {
+			printf("%c", cur_char.value);
+			if (cur_char.next != NULL) 
+				cur_char = *cur_char.next;
+			else break;
+		}
+
+
+		printf("\n");
+	}
+}
+
+void clean_buffer() {
+	while (getchar() != '\n');
+	getchar();
+
 }
