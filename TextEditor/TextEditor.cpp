06372fc84db69613043322c351c3d337d1af97a4
Nina Rosliakova <nrosliakova@kse.org.ua>
2025-05-29 23:25:34 +0300

    2. Rewrite the existing code by using object-oriented approach DONE!

--- TextEditor/TextEditor.cpp
+++ TextEditor/TextEditor.cpp
@@ -1,7 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//-- c++
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 typedef struct node{
 	char value = NULL;
@@ -13,34 +17,198 @@ typedef struct coordinates{
 	int char_num = NULL;
 } coordinates;
 
-//node* resize(node* ptr, int new_size);
-node** resize();
-coordinates* resize_coordinates(coordinates* ptr, int new_size);
-void append(const char text[]);
-node* insert(const char text[], node* nd);
-coordinates* search(const char text[]);
-void print_text();
-void print_text_in_file(FILE* fptr);
-void clean_buffer();
+class TextEditor {
+	int arraysize = 4;
+	node** p_first_chars;
+	//node* p_last_chars;
+	node* last_char;
+	int line_counter = 0;
 
-int arraysize = 4;
-node** p_first_chars;
-//node* p_last_chars;
-node* last_char;
-int line_counter = 0;
+	public:
+		TextEditor() {
+			p_first_chars = (node**)calloc(arraysize, sizeof(node));
+			for (int i = 0; i < arraysize; i++) {
+				p_first_chars[i] = NULL;
+			}
+
+			last_char = p_first_chars[0];
+		}
+		~TextEditor() {
+			/*node* cur_char = p_first_chars[0];
+			while (cur_char != NULL) {
+				node* next_char = cur_char->next;
+				free(cur_char);
+				cur_char = next_char;
+			}
+			free(p_first_chars);
+			p_first_chars = NULL;
+			free(last_char);
+			last_char = NULL;*/
+		}
+		//----------------------FUNCTIONS--------------------------------------------
+		
+		char get_input(const char text[]) {
+
+		}
+
+		void append(const char text[]) {
+			int len = strlen(text);
+			node* nodes = (node*)calloc(len + 1, sizeof(node));
+			if (last_char != NULL && last_char->value != NULL) {
+				last_char->next = &nodes[0];
+			}
+
+			for (int i = 0; i < len; i++) {
+				node* cur_node_p;
+				cur_node_p = &nodes[i];
+				cur_node_p->value = text[i];
+
+				node* node_next_p = &nodes[i + 1];
+				cur_node_p->next = (i != len - 1) ? node_next_p : NULL;
+				if (p_first_chars[line_counter] == NULL || p_first_chars[line_counter]->value == NULL)
+					p_first_chars[line_counter] = cur_node_p;
+
+
+				last_char = cur_node_p;
+			}
+		}
+
+		void new_line() {
+			append("\n");
+			line_counter++;
+			if (line_counter > arraysize)
+				p_first_chars = resize();
+		}
+
+		node* insert(const char text[], node* nd) {
+			int len = strlen(text);
+			node* nodes = (node*)calloc(len + 1, sizeof(node));
+			node* prev_node = nd;
+			node* node_after_insert = nd->next;
+			prev_node->next = &nodes[0];
+
+			for (int i = 0; i < len; i++) {
+				node* cur_node_p;
+				cur_node_p = &nodes[i];
+				cur_node_p->value = text[i];
+
+				node* node_next_p = &nodes[i + 1];
+				cur_node_p->next = (i != len - 1) ? node_next_p : NULL;
+
+				prev_node = cur_node_p;
+			}
+			prev_node->next = node_after_insert;
+			return nd;
+		}
+		 
+		void search(const char text[]) {
+			node cur_char = *p_first_chars[0];
+			int coord_arr_size = 4;
+			coordinates* coord_arr = (coordinates*)calloc(coord_arr_size, sizeof(coordinates));
+			int el_counter = 0;
+			int len_ = strlen(text);
+			int i = 0;
+			int cur_line_num = 0;
+			int cur_char_num = 0;
+			coordinates coord;
+
+			while (cur_char.value != NULL) {
+				if (cur_char.value == '\n') {
+					cur_line_num++;
+					cur_char_num = 0;
+					if (cur_char.next != NULL) cur_char = *cur_char.next;
+					continue;
+				}
+				if (cur_char.value == text[i]) {
+					if (i == 0) {
+						coord.char_num = cur_char_num;
+						coord.line_num = cur_line_num;
+					}
+					i++;
+				}
+				else i = 0;
+
+				if (i == len_) {
+					if (el_counter + 1 > coord_arr_size) {
+						coord_arr_size *= 2;
+						coord_arr = resize_coordinates(coord_arr, coord_arr_size);
+					}
+					coord_arr[el_counter] = coord;
+					el_counter++;
+					i = 0;
+				}
+				if (cur_char.next != NULL) {
+					cur_char = *cur_char.next;
+					cur_char_num++;
+				}
+				else break;
+			}
+			printf("\nText is present in positions (%d results): \n\n", el_counter);
+			printf("\tline\tchar\n");
+			for (int j = 0; j < el_counter; j++) {
+				printf("\t%d\t%d\n", coord_arr[j].line_num, coord_arr[j].char_num);
+			}
+		}
+
+		void print_text() {
+			node cur_char = *p_first_chars[0];
+			while (cur_char.value != NULL) {
+				printf("%c", cur_char.value);
+				if (cur_char.next != NULL)
+					cur_char = *cur_char.next;
+				else break;
+			}
+		}
+		void print_text_in_file(FILE* fptr) {
+			node cur_char = *p_first_chars[0];
+			while (cur_char.value != NULL) {
+				//printf("%c", cur_char.value);
+				fprintf(fptr, "%c", cur_char.value);
+				if (cur_char.next != NULL)
+					cur_char = *cur_char.next;
+				else break;
+			}
+		}
+
+		void clean_buffer() {
+			while (getchar() != '\n');
+			getchar();
+
+		}
+		node* find_char(int line_num, int char_num) {
+			if (line_num > line_counter) {
+				printf("Error, this line does not exist yet. ");
+				//return false;
+				return NULL;
+			}
+			node* nod = p_first_chars[line_num];
+			bool error_occurred = false;
+			for (int i = 1; i <= char_num; i++) {
+				if (nod->next == NULL) {
+					printf("Error, this character number does not exist yet. Last character number is %d.", i);
+					return NULL;
+				}
+				nod = nod->next;
+			}
+			return nod;
+		}
+	private:
+		node** resize() {
+			arraysize *= 2;
+			node** ptr2 = (node**)realloc(p_first_chars, arraysize * sizeof(node));
+			return ptr2;
+		}
+		coordinates* resize_coordinates(coordinates* ptr, int new_size) {
+			coordinates* ptr2 = (coordinates*)realloc(ptr, new_size * sizeof(coordinates));
+			return ptr2;
+		}
+		
+};
 
 int main() {
-
-	p_first_chars = (node**)calloc(arraysize, sizeof(node));
-	for (int i = 0; i < arraysize; i++) {
-		//node* n;
-		p_first_chars[i] = NULL;
-	}
-
-	//---
-	last_char = p_first_chars[0];
 	printf("Hello User!\n");
 	char command;
+	TextEditor text_editor;
 
 	while (1) {
 		printf("Choose the command (press 'h' for help) : ");
@@ -71,23 +239,22 @@ int main() {
 					text[len - 1] = '\0';
 				}
 				text[len] = '\0';
-				append(text);
+				text_editor.append(text);
 				printf("\n");
 				printf("Done! Text to append:\n%s\n", text);
 				break;
 			}
 			case '2': // Start the new line
-				append("\n");
-				line_counter++;
-				if (line_counter > arraysize)
-					p_first_chars = resize();
+				text_editor.new_line();
 				printf("Done!\n");
 				break;
 			case '3': // Use files to saving the information
 				FILE * fptr;
 				fopen_s(&fptr, "C:\\Dev\\KSE\\1 year\\Principles and Paradigms\\Assignments\\Simple Text Editor\\TextEditorResult.txt", "w");
-				print_text_in_file(fptr);
+				text_editor.print_text_in_file(fptr);
 				fclose(fptr);
+				free(fptr);
+				fptr = NULL;
 
 				printf("Text has been saved successfully!\n");
 				break;
@@ -112,8 +279,7 @@ int main() {
 					
 					char text[100];
 					while (fgets(text, 100, fptr) != NULL) {
-						append("\n");
-						line_counter++;
+						text_editor.new_line();
 
 						int len = strlen(text);
 						if (len > 0 && text[len - 1] == '\n') {
@@ -121,21 +287,21 @@ int main() {
 						}
 						text[len] = '\0';
 
-						append(text);
+						text_editor.append(text);
 
 					}
-
-
 					printf("\n");
 					printf("Text has been loaded successfully!\n");
 					fclose(fptr);
+					free(fptr);
+					fptr = NULL;
 				}
 				break;
 			}
 			case '5': // Print the current text to console 
 			{
 				printf("Your text:\n\n");
-				print_text();
+				text_editor.print_text();
 				printf("\n\n");
 
 				break;
@@ -149,22 +315,8 @@ int main() {
 				scanf_s("%d %d", &line_num, &char_num);
 				printf("\n");
 
-				if (line_num > line_counter) {
-					printf("Error, this line does not exist yet. ");
-					break;
-				}
-				node* node_before_insert = p_first_chars[line_num];
-				bool error_occurred = false;
-				for (int i = 1; i <= char_num; i++) {
-					if (node_before_insert->next == NULL) {
-						printf("Error, this character number does not exist yet. Last character number is %d.", i);
-						error_occurred = true;
-						break;
-					}
-					node_before_insert = node_before_insert->next;
-				}
-				if (error_occurred) break;
-
+				node* node_before_insert = text_editor.find_char(line_num, char_num);
+				if (node_before_insert == NULL) break;
 				char text[100];
 				printf("Enter text to insert (up to 100 characters):\n\n");
 				while (getchar() != '\n');
@@ -174,7 +326,8 @@ int main() {
 					text[len - 1] = '\0';
 				}
 				text[len] = '\0';
-				node_before_insert = insert(text, node_before_insert);
+				//node_before_insert = text_editor.insert(text, node_before_insert);
+				text_editor.insert(text, node_before_insert);
 
 				break;
 			}
@@ -190,54 +343,7 @@ int main() {
 				}
 				text[len] = '\0';
 	
-				//-----------------------------------------------------------------
-				node cur_char = *p_first_chars[0];
-				int coord_arr_size = 4;
-				coordinates* coord_arr = (coordinates*)calloc(coord_arr_size, sizeof(coordinates));
-				int el_counter = 0;
-				int len_ = strlen(text);
-				int i = 0;
-				int cur_line_num = 0;
-				int cur_char_num = 0;
-				coordinates coord;
-
-				while (cur_char.value != NULL) {
-					if (cur_char.value == '\n') {
-						cur_line_num++;
-						cur_char_num = 0;
-						if (cur_char.next != NULL) cur_char = *cur_char.next;
-						continue;
-					}
-					if (cur_char.value == text[i]) {
-						if (i == 0) {
-							coord.char_num = cur_char_num;
-							coord.line_num = cur_line_num;
-						}
-						i++;
-					}
-					else i = 0;
-
-					if (i == len_) {
-						if (el_counter + 1 > coord_arr_size) {
-							coord_arr_size *= 2;
-							coord_arr = resize_coordinates(coord_arr, coord_arr_size);
-						}
-						coord_arr[el_counter] = coord;
-						el_counter++;
-						i = 0;
-					}
-					if (cur_char.next != NULL) {
-						cur_char = *cur_char.next;
-						cur_char_num++;
-					}
-					else break;
-				}
-				//-----------------------------------------------------------------
-				printf("\nText is present in positions (%d results): \n\n", el_counter);
-				printf("\tline\tchar\n");
-				for(int j = 0; j < el_counter; j++){
-					printf("\t%d\t%d\n", coord_arr[j].line_num, coord_arr[j].char_num);
-				}
+				text_editor.search(text);
 				break;
 			}
 			case 'e':
@@ -248,90 +354,11 @@ int main() {
 		printf("\nPress enter to continue... ");
 		//while (getchar() != '\n');
 		//getchar();
-		clean_buffer();
+		text_editor.clean_buffer();
 		system("cls");
 		//while (getchar() != '\n');
 	}
 	
 	
 	return 0;
-}
-
-node** resize() {
-	arraysize *= 2;
-	node** ptr2 = (node** )realloc(p_first_chars, arraysize * sizeof(node));
-	return ptr2;
-}
-coordinates* resize_coordinates(coordinates* ptr, int new_size) {
-	arraysize *= 2;
-	coordinates* ptr2 = (coordinates* )realloc(ptr, new_size * sizeof(coordinates));
-	return ptr2;
-}
-
-void append(const char text[]) {
-	int len = strlen(text);
-	node* nodes = (node*)calloc(len + 1, sizeof(node));
-	if (last_char != NULL && last_char->value != NULL) {
-		last_char->next = &nodes[0];
-	}
-
-	for (int i = 0; i < len; i++) {
-		node* cur_node_p;
-		cur_node_p = &nodes[i];
-		cur_node_p->value = text[i];
-
-		node* node_next_p = &nodes[i + 1];
-		cur_node_p->next = (i != len - 1) ? node_next_p : NULL;
-		if (p_first_chars[line_counter] == NULL || p_first_chars[line_counter]->value == NULL)
-			p_first_chars[line_counter] = cur_node_p;
-				
-
-		last_char = cur_node_p;
-	}
-}
-node* insert(const char text[], node* nd) {
-	int len = strlen(text);
-	node* nodes = (node*)calloc(len + 1, sizeof(node));
-	node* prev_node = nd;
-	node* node_after_insert = nd->next;
-	prev_node->next = &nodes[0];	
-
-	for (int i = 0; i < len; i++) {
-		node* cur_node_p;
-		cur_node_p = &nodes[i];
-		cur_node_p->value = text[i];
-
-		node* node_next_p = &nodes[i + 1];
-		cur_node_p->next = (i != len - 1) ? node_next_p : NULL;			
-
-		prev_node = cur_node_p;
-	}
-	prev_node->next = node_after_insert;
-	return nd;
-}
-
-void print_text() {
-	node cur_char = *p_first_chars[0];
-	while (cur_char.value != NULL) {
-		printf("%c", cur_char.value);
-		if (cur_char.next != NULL)
-			cur_char = *cur_char.next;
-		else break;
-	}
-}
-void print_text_in_file(FILE * fptr) {
-	node cur_char = *p_first_chars[0];
-	while (cur_char.value != NULL) {
-		//printf("%c", cur_char.value);
-		fprintf(fptr, "%c", cur_char.value);
-		if (cur_char.next != NULL)
-			cur_char = *cur_char.next;
-		else break;
-	}
-}
-
-void clean_buffer() {
-	while (getchar() != '\n');
-	getchar();
-
 }
